86869ac4c60d22d0079b6539ae67a35c9099c651
PC1011 <PC1011@DESKTOP-52EVHMJ>
2026-01-15 20:52:01 +0300

    Karşılaştırma operatörleri işlendi

--- 260115_2_Karsilastirma_Operatorleri/Program.cs
+++ 260115_2_Karsilastirma_Operatorleri/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-           
+            /* 2) Karşılaştırma operatörleri:
+             * == eşitmi,
+             * != eşit değilmi,
+             * > büyükmü,
+             * < küçükmü
+             * >= büyük eşitmi
+             * <= küçük eşitmi
+             */
+
+            int sayi1 = 45;
+            int sayi2 = 30;
+            string metin1 = "yazılım";
+            DateTime tarih = DateTime.Now;
+
+            bool sonuc = sayi1 == 10; //false
+            Console.WriteLine("45=10 =>"+sonuc);
+
+            sonuc = sayi1 != sayi2;
+            Console.WriteLine("45!=30"+sonuc);
+
+            Console.WriteLine("10>=5:"+(10>=5));
+            Console.WriteLine("10>5:"+(10>5));
+            sonuc = metin1 == "software";//false
+            Console.WriteLine("yazılım==software:"+sonuc);
+            Console.WriteLine("01.01.2026 01:20:00:" + tarih + "=>" + (Convert.ToDateTime("01.01.2026 01:20:00")==tarih));
+
+
         }
     }
 }
