e5a9fc67c3551b05d68c07b0fb1c32caad3d8ec9
PC1011 <PC1011@DESKTOP-52EVHMJ>
2026-01-15 21:33:50 +0300

    Ornek yapıldı

---
+++ 260115_4_Ornek_Operatorler/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _260115_4_Ornek_Operatorler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool sonuc;
+
+            sonuc = 14 >= 14;
+            Console.WriteLine("14>=14 sonuç:" + sonuc);
+            sonuc = 14 >= 14 || (45 == 45);
+            Console.WriteLine("14>=14 || (45==45) sonuç:" + sonuc);
+            sonuc = 14 >= 14 || (45 == 45 && "software" == "yazılım");
+            Console.WriteLine("14>=14 && (45==45 && \"software\"==\"yazılım\")sonuç:" + sonuc);
+            sonuc = "a" != "A" && (14 >= 4 || (DateTime.Now.Year == 2026 && true != false));
+            sonuc = "Yaşar Kemal Van" != "Orhan Pamuk İstanbul" && (14.5 == Convert.ToDouble("14.5") || false == true ) && (double)45 == Convert.ToDouble("45");
+
+
+        }
+    }
+}
