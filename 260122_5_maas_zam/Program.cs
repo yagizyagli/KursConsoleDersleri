19d15e99be827dff96368013456a07651d9c028f
Yağız Yağlı <yagizyagli3447@gmail.com>
2026-01-23 18:01:01 +0300

    Update Program.cs

--- 260122_5_maas_zam/Program.cs
+++ 260122_5_maas_zam/Program.cs
@@ -9,9 +9,39 @@
                 Eğer maaş değeri 25.000 ve altında ise %45 zam
                 Eğer maaş 50.000 ve üstünde ise %17 zam
                 Eğer maaş 100.000 ve üstünde ise %9 zam
-                Eğer maaş 150.000 ve üstü ise %5 zam yapacak şekilde yapılan zam oranını ve yeni maaşı kullanıcıya gösteren yapıyı kodlayınız
-             
+                Eğer maaş 150.000 ve üstü ise %5 zam yapacak şekilde yapılan zam oranını ve yeni maaşı kullanıcıya gösteren yapıyı kodlayınız             
              */
+
+            START:
+    Console.WriteLine("Maaşınızı giriniz:");
+    double maas = Convert.ToDouble(Console.ReadLine());
+
+    if(maas<=25000)
+    {                
+        maas += maas * 45 / 100;
+        Console.WriteLine("Yeni Maaşınız: "+maas);
+    }
+    
+    else if(maas>=50000)
+    {
+        maas += maas * 17 / 100;
+        Console.WriteLine("Yeni Maaşınız: "+maas);
+    }
+    else if(maas >= 100000)
+    {
+        maas+= maas *9 / 100;
+        Console.WriteLine("Yeni Maaşınız: " + maas);
+    }
+    else if(maas >= 150000)
+    {
+        maas+= maas *5 / 100;
+        Console.WriteLine("Yeni Maaşınız: " + maas);
+    }
+    else
+    {
+        Console.WriteLine("Hatalı Maaş");
+    }
+    goto START;
         }
     }
 }
