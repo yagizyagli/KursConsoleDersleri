7a0383a3e61b385c28e84c41a20713ac0f594b39
PC1011 <PC1011@DESKTOP-52EVHMJ>
2026-01-27 20:48:00 +0300

    while asal sayı süper sayı

--- 260123_6_while_ornek2/Program.cs
+++ 260123_6_while_ornek2/Program.cs
@@ -8,11 +8,37 @@
              1)1-100 arasındaki sayıları ekrana yazın ve toplalarını hesaplayın
             2)55-255 arasındaki 5 in katları ekrana yazın ve toplamlarını hesaplayın
             3)500 den 0 a doğru 13 katlarını ekranda yazınız(tersten sayım olacak)
-            4)1-100 arasında asal sayıların toplamını hesaplayınız(zor soru)
-             
+            4)1-100 arasında asal sayıların toplamını hesaplayınız(zor soru)         
              */
 
-            
+
+            int sayi2 = 2;
+            //bir sayı 1 ve kendisinden başka bölüneni yoksa asaldır
+            int toplamAsalSayilar = 0;
+            while (sayi2 < 1000)
+            {
+                int bolenSayi = 2;
+                bool asalMi = true;
+                while (sayi2 > bolenSayi)
+                {
+                    if (sayi2 % bolenSayi == 0)
+                    {
+                        asalMi = false;
+                        break;//içinde bulunduğu döngüyü sonlandırır.sayi2>bolenSayi koşullu döngüyü sonlandırır,diğer döngüye devam eder.
+                    }
+                    bolenSayi++;
+                }
+                if (asalMi)
+                {
+                    toplamAsalSayilar = toplamAsalSayilar + sayi2;
+                    Console.Write(sayi2 + ",");
+                }
+                sayi2++;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Toplam Asal Sayılar: "+toplamAsalSayilar);
         }
+        
+        
     }
 }
---
+++ 260127_1_Super_sayi_while/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _260127_1_Super_sayi_while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //kendisi hariç bölümlerin toplamı kendisini veren sayılara süper sayı denir.1-100000 arasındaki süper sayıları listeleyiniz ?
+            int sayi1 = 0;                        
+            while (sayi1<100000)
+
+            {
+                int bolensayi = 1;
+                int toplam = 0;
+                while (sayi1 > bolensayi)
+                {
+                    if (sayi1 % bolensayi == 0)
+                    {
+                        toplam = toplam + bolensayi;
+
+                    }
+                    bolensayi++;
+                }
+               
+                if (sayi1 == toplam)
+                {
+                    Console.WriteLine(sayi1);
+                }
+
+                sayi1++;
+            }
+            
+        }
+    }
+}
